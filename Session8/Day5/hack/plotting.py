07da959f9cf56e9b4f428058fc8a6b91657c6d93
Robert Morgan <robert.morgan@wisc.edu>
2019-06-10 08:38:11 -0400

    finish adding Session 8 commits

---
+++ Session8/Day5/hack/plotting.py
@@ -0,0 +1,15 @@
+#A code to set plotting defaults
+
+import matplotlib as mpl
+
+#Axes
+mpl.rcParams['axes.linewidth'] = 2
+mpl.rcParams['xtick.labelsize'] = 14
+mpl.rcParams['ytick.labelsize'] = 14
+mpl.rcParams['xtick.major.size'] = 6
+mpl.rcParams['ytick.major.size'] = 6
+
+#Fonts
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.size'] = 14
+mpl.rcParams['font.weight'] = 'medium'
